41d6fc7b0663bb8645a1ddbe1cac23ad33c4c89a
muhammadtalhanizam <hafiztalhanizam@gmail.com>
2022-03-23 21:01:39 +0500

    work in progress

---
+++ JS Chap#1 to 13/app.js
@@ -0,0 +1,206 @@
+//////  Chap# 1 (ALERT)  //////
+
+// alert("Error! Please Enter a valid Password.");
+
+// alert("Welcome to JS Land...\n Happy Coding!" );
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+
+
+//////  Chap# 2 (VARIABLES FOR STRINGS)  //////
+
+
+// var username;
+
+
+// var myName = "Talha Nizam";
+
+
+// var message = "Hello World";
+// alert(message);
+
+
+// var age = 20 + " years old";
+// var certification = "Certified Web and Mobile Application Development";
+// alert(myName);
+// alert(age);
+// alert(certification);
+
+
+// var pizza = "PIZZA\n" + "PIZZ\n" + "PIZ\n" + "PI\n" + "P" ;
+// alert(pizza);
+
+
+// var email = "hafiztalhanizam36@gmail.com";
+// alert("My email address is " + email);
+
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+// document.write("Yah! I can write HTML content through JavaScript.");
+
+
+// var style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(style);
+
+
+
+//////  Chap# 3 (VARIABLES FOR NUMBERS)  //////
+
+
+// var age = 20;
+// alert("I am " + age + " years old" );
+
+
+// var N = 14;
+// alert("You have visited this site " + N + " times");
+
+
+// var birthYear = 2001;
+// document.write("My birth year is " + birthYear + "\n Data type of my declared variable is number");
+
+
+// var visitorName = "John Doe";
+// var quantity = 5;
+// var productTitle = "T-shirt";
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
+
+
+
+//////  Chap# 4 (VARIABLE NAMES: LEGAL & ILLEGAL)  //////
+
+
+// var var1="1st Variable", var2="2nd Variable.", var3= "3rd Variable.";
+
+
+// var $1var, _var1, Var1, var1, VAR1;
+// var 1var, -var1, .var1, %var1, ^var1;
+
+
+// document.write("Rules for naming JS variables <br> <br>Variable names can only contain numbers, $ and _. For example $my_1stVariable. <br>Variables must begin with a letter, $ or For example $name, _name or name. <br>Variable names are case sensitive. <br>Variable names should not be JS keywords.");
+
+     
+
+//////  Chap# 5 (MATH EXPRESSIONS)  //////
+
+//* Task # 1 & 2 *//
+  
+// var num1 = 3;
+// var num2 = 5;
+
+// sum = num1 + num2;
+// document.write("Sum of " +num1+ " and " +num2+ " is " +sum +"<br>");
+
+
+// difference = num2 - num1;
+// document.write("Diffrence of " +num2+ " and " +num1+ " is " +difference +"<br>");
+
+
+// product = num1 * num2;
+// document.write("Product of " +num1+ " and " +num2+ " is " +product +"<br>");
+
+
+// quotient = num2 / num1;
+// document.write("Quotient of " +num2+ " and " +num1+ " is " +quotient +"<br>");
+
+
+// modulus = num2 % num1;
+// document.write("Modulus of " +num2+ " and " +num1+ " is " +modulus+"<br>");
+
+
+//* Task # 3 *//
+
+// var value = 5;
+// document.write("<br> Initial Value: " +value);
+
+// value++;
+// document.write("<br> Value after increment is: " +value);
+
+// value = value+7;
+// document.write("<br> Value after addition is: " +value);
+
+// value--;
+// document.write("<br> Value after increment is: " +value);
+
+// value = value%3;
+// document.write("<br> The remainder is : " +value);
+
+
+//* Task # 4 *//
+
+// var ticketPrice = 600;
+// var totalcost = ticketPrice*5;
+// document.write("Total cost to buy 5 tickets to a movie is " +totalcost+"PKR");
+
+
+//* Task # 5 *//
+
+// var table = 4;
+// var num1 = table*1,
+//     num2 = table*2,
+//     num3 = table*3,
+//     num4 = table*4,
+//     num5 = table*5,
+//     num6 = table*6,
+//     num7 = table*7,
+//     num8 = table*8,
+//     num9 = table*9,
+//     num10 = table*10;
+
+// document.write("Table of 4 <br>"
+                    
+//                     +table+ "x 1 =" +num1+ "<br>"
+//                     +table+ "x 2 =" +num2+ "<br>"
+//                     +table+ "x 3 =" +num3+ "<br>"
+//                     +table+ "x 4 =" +num4+ "<br>"
+//                     +table+ "x 5 =" +num5+ "<br>"
+//                     +table+ "x 6 =" +num6+ "<br>"
+//                     +table+ "x 7 =" +num7+ "<br>"
+//                     +table+ "x 8 =" +num8+ "<br>"
+//                     +table+ "x 9 =" +num9+ "<br>"
+//                     +table+ "x 10 =" +num10+ "<br>"
+//                     );
+                    
+
+
+//* Task # 6 *//
+
+// var C = 25;
+// var CToF = (C * 9/5) +32;
+
+// document.write(C+"°C is "+CToF +"°F <br>");
+
+
+// var F = 70;
+// var FToC = (F - 32) *5/9;
+
+// document.write(F+"°F is "+FToC +"°C");
+
+
+
+//* Task # 7 *//
+
+// var pItem1= 650,
+//     qItem1= 3,
+//     pItem2= 100,
+//     qItem2= 7,
+//     shipCharges = 100;
+
+// var totalCost = (pItem1 * qItem1) + (pItem2 * qItem2) + shipCharges;
+// document.write(
+//     "<br> Price of item 1 is "+ pItem1+
+//     "<br> Quantity of item 1 is "+ qItem1+
+//     "<br> Price of item 2 is "+ pItem2+
+//     "<br> Quantity of item 2 is "+ qItem2+
+//     "<br> Shipping Charges is "+ shipCharges+
+//     "<br> Total Cost of Your Order is: "+totalCost);
+
+
+
+
+
+
